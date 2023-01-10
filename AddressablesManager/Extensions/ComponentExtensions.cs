d21a0b0e33c267e52e278ee2b6c1b11577030439
Ca <ca@wolffungame.com>
2023-01-10 16:17:17 +0700

    Dock to GameObject

---
+++ AddressablesManager/Extensions/ComponentExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace UnityEngine.AddressableAssets
+{
+    public static class ComponentExtensions
+    {
+        // Util.
+        public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+        {
+#if UNITY_2019_2_OR_NEWER
+            if (!gameObject.TryGetComponent<T>(out var component))
+                component = gameObject.AddComponent<T>();
+
+#else
+            var component = gameObject.GetComponent<T>();
+            if (component == null)
+                component = gameObject.AddComponent<T>();
+#endif
+
+            return component;
+        }
+    }
+}
---
+++ AddressablesManager/SuportComponent/DestroyTriggerComp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UnityEngine.AddressableAssets
+{
+    public class DestroyTriggerComp : MonoBehaviour
+    {
+        internal Action OnGameObjectDestroy;
+
+        private void OnDestroy() => OnGameObjectDestroy?.Invoke();
+    }
+}
